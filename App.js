7212def51eedb60f3f16ea988f7745d49a374f6c
mujeebalish <mujeebrehman12122@gmail.com>
2024-11-03 11:11:28 -0800

    ok

---
+++ App.js
@@ -0,0 +1,19 @@
+var sellDate = new Date(2024,10,11,12,00,00,999).getTime();
+var currentDate = new Date().getTime();
+var reamaningTime = sellDate-currentDate;
+var getDays = Math.floor(reamaningTime/(1000*60*60*24));
+var gethours = Math.floor((reamaningTime%(1000*60*60*24))/(1000*60*60));
+var getminutes = Math.floor((reamaningTime%(1000*60*60))/(1000*60));
+var getseconds = Math.floor((reamaningTime%(1000*60))/(1000));
+
+document.getElementById("days-box").innerHTML =  "0" + getDays + "<br>" + "DAYS";
+document.getElementById("hours-box").innerHTML =  "0" + gethours + "<br>" + "HOURS";
+document.getElementById("minutes-box").innerHTML = getminutes + "<br>" + "MINUTES";
+document.getElementById("second-box").innerHTML =  getseconds + "<br>" + "SECONDS";
+
+console.log(getDays);
+
+
+
+
+
